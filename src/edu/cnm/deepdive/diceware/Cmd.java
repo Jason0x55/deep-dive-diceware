83b76a312b43ebbdec8c14b97412ec40acb84067
Jason Rafferty <Jason0x55@gmail.com>
2018-06-03 22:45:48 -0600

    Added JavaDoc for Cmd class.

--- src/edu/cnm/deepdive/diceware/Cmd.java
+++ src/edu/cnm/deepdive/diceware/Cmd.java
@@ -8,11 +8,19 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This class is a basic command line interface for the Generator class.
+ */
 public class Cmd {
 
   private static final String DEFAULT_FILENAME = "wordlist.txt";
   private static final int DEFAULT_NUMBER_WORDS = 6;
 
+  /**
+   * Takes command line args and prints out random words.
+   * @param args Takes two optional arguments. Defaults to wordlist.txt and 6 words total.
+   * @throws FileNotFoundException Thrown if there is an issue loading the file.
+   */
   public static void main(String[] args) throws FileNotFoundException {
     int numWords = DEFAULT_NUMBER_WORDS;
     String fileName = DEFAULT_FILENAME;
@@ -32,6 +40,12 @@ public class Cmd {
     System.out.println("");
   }
 
+  /**
+   * Reads in a wordlist and places them into an array to be passed to the Generator class.
+   * @param filename Name of file to be loaded.
+   * @return Returns an array of the wordlist.
+   * @throws FileNotFoundException Thrown if there is an issue loading the file.
+   */
   private static String[] readWordList(String filename) throws FileNotFoundException {
     try (Scanner scanner = new Scanner(new File(filename))) {
       List<String> words = new LinkedList<>();
